403049aad2da83b61d3de5c44d7992462728192c
Jayaprakash0003 <lvurself03@gmail.com>
2024-08-02 01:23:44 +0530

    4th part completed socket

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async(req,res)=>{
     try{
@@ -21,11 +22,18 @@ export const sendMessage = async(req,res)=>{
         receiverId,
         message,
        });
+
        if(newMessage){
         conversation.messages.push(newMessage._id);
        }
     //    await conversation.save();
     //    await newMessage.save();
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+        // io.to(<socket_id>).emit() used to send events to specific client
+        io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
        await Promise.all([conversation.save(),newMessage.save()]);
        res.status(201).json(newMessage);
     }catch(error){
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./routes/connentToMongoDB.js";
 import cookieParser from "cookie-parser";
-const app = express();
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT,() => {
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
     connectToMongoDB();
-console.log(`server Running on port ${PORT}`)});
-
-
-
-    
+    console.log(`Server running on port ${PORT}`);
+});
